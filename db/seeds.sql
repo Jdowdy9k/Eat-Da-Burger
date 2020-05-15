416fd09a58b3bfaf18e3854e349a0a959ceba933
Jdowdy <Jdowdy9k@gamil.com>
2020-05-15 15:21:54 -0400

    added dependancies and database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('The Kale Rider veggie burger');
+INSERT INTO burgers (burger_name) VALUES ('The Golber of fire turkey burger');
+INSERT INTO burgers (burger_name) VALUES ('pineapple teriyaki patty melt', true);
+INSERT INTO burgers (burger_name) VALUES ('cheesed to meat you! burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Slammin Salmon burger', true);
+
